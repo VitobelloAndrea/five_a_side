383c400b9ae6ccc961a39290c5376192ac8e47c2
VitobelloAndrea <andrea.vitobello@mail.polimi.it>
2021-10-16 10:25:01 +0200

    moved the team formation code inside the Roster class and updated the players' values
    also added the "printPlayersByAverage" to make fun of GG

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -19,116 +19,22 @@ public class App
         Gson gson = new Gson();
         Roster roster = gson.fromJson(new FileReader("src/Json/roster.json"), Roster.class);
 
+        //teamFormation(roster);
+
+        //roster.printPlayersByAverage();
+    }
+
+    static void teamFormation(Roster roster){
         String[] players = {"Cima", "Paolino", "Marco7Miglia", "Ale", "Deda", "Conte", "Pippo", "Guerra", "Vito", "Simo"};
-        Player[] nextGame = new Player[10];
-        int counter = 0;
-        for (int i = 0; i < roster.getRoster().length; i++){
-            for (int j = 0; j < players.length; j++) {
-                Player selected = roster.getRoster()[i];
-                if (selected.getName().equals(players[j])) {
-                    nextGame[counter] = new Player(players[j], selected.getES(), selected.getBS(), selected.getSHO(), selected.getDEF(), selected.getPHY(), selected.getCON());
-                    counter += 1;
-                }
-            }
-        }
-
-        /*
-        System.out.println();
-        for (int i = 0; i < nextGame.length; i++)
-            System.out.println(nextGame[i].getName() + " : " + nextGame[i].getAVG());
-         */
-
-        int ar[] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
-        shuffleArray(ar);
-
         Team team1 = new Team();
         Team team2 = new Team();
-        for (int i = 0; i < ar.length; i++){
-            if (i < 5) team1.setPlayer( i, nextGame[ar[i]]);
-            else team2.setPlayer(i-5, nextGame[ar[i]]);
-        }
-
-        System.out.println("\nTEAM1-AVG: " + team1.getTotal() + "\nTEAM2-AVG: " + team2.getTotal());
-
-        Random rand = new Random();
-        while (team2.getTotal() > team1.getTotal() + 10 || team2.getTotal() < team1.getTotal() - 10){
-            int index1 = rand.nextInt(5);
-            int index2 = rand.nextInt(5);
-            team1.switchPlayers(index1, index2, team2);
-        }
+        roster.makeTeams(players, team1, team2);
 
         System.out.println("\n\n\t\t---FINAL TEAMS---");
         team1.printTeam();
         System.out.println();
         team2.printTeam();
         System.out.println("\nTEAM1-AVG: " + team1.getTotal() + "\nTEAM2-AVG: " + team2.getTotal());
-
-        /*
-        for (int i = 0; i < ar.length; i++)
-            System.out.print(ar[i] + ", ");
-        System.out.println();
-
-        Player[] team1 = new Player[5];
-        Player[] team2 = new Player[5];
-        for (int i = 0; i < ar.length; i++){
-            if (i < 5) team1[i] = nextGame[ar[i]];
-            else team2[i-5] = nextGame[ar[i]];
-        }
-
-        int team1TOT = 0;
-        int team2TOT = 0;
-
-        for (int i = 0; i < team1.length; i++) {
-            team1TOT += team1[i].getAVG();
-        }
-
-        for (int i = 0; i < team2.length; i++) {
-            team2TOT += team2[i].getAVG();
-        }
-
-        System.out.println("\n\nTEAM1-AVG: " + team1TOT + "\nTEAM2-AVG: " + team2TOT);
-
-        Random rand = new Random();
-        while (team2TOT > team1TOT + 10 || team2TOT < team1TOT - 10){
-            int index1 = rand.nextInt(5);
-            int index2 = rand.nextInt(5);
-            team1TOT -= team1[index1].getAVG();
-            team2TOT -= team2[index2].getAVG();
-            Player temp = team1[index1];
-            team1[index1] = team2[index2];
-            team2[index2] = temp;
-            team1TOT += team1[index1].getAVG();
-            team2TOT += team2[index2].getAVG();
-        }
-
-
-        System.out.println("\n\n\t\t---FINAL TEAMS---");
-        System.out.println("\nTeam 1:");
-        for (int i = 0; i < team1.length; i++) {
-            System.out.println(team1[i].getName() + " : " + team1[i].getAVG());
-        }
-
-        System.out.println("\nTeam 2:");
-        for (int i = 0; i < team2.length; i++) {
-            System.out.println(team2[i].getName() + " : " + team2[i].getAVG());
-        }
-
-        System.out.println("\nTEAM1-AVG: " + team1TOT + "\nTEAM2-AVG: " + team2TOT);
-        */
     }
 
-
-    static void shuffleArray(int[] ar)
-    {
-        // If running on Java 6 or older, use `new Random()` on RHS here
-        Random rnd = new Random();
-        for (int i = ar.length - 1; i > 0; i--)
-        {
-            int index = rnd.nextInt(i + 1);
-            // Simple swap
-            int a = ar[index];
-            ar[index] = ar[i];
-            ar[i] = a;
-        }
-    }
 }
--- src/main/java/org/example/Roster.java
+++ src/main/java/org/example/Roster.java
@@ -1,5 +1,9 @@
 package org.example;
 
+import java.util.HashSet;
+import java.util.Random;
+import java.util.Set;
+
 public class Roster {
     private Player[] roster;
 
@@ -14,6 +18,72 @@ public class Roster {
     public void getAverages(){
         for (int i = 0; i < roster.length; i++){
             System.out.println(roster[i].getName() + " : " + roster[i].getAVG());
+        }
+    }
+
+    public void printPlayersByAverage(){
+        Player[] orderedRoster = new Player[roster.length];
+        Set<String> alreadyAdded = new HashSet<>();
+        for (int i = 0; i < roster.length; i++){
+            int index = -1;
+            double maxAvg = 0;
+            String maxPlayer = "";
+            for (int j = 0; j < roster.length; j++){
+                if (!alreadyAdded.contains(roster[j].getName()) && maxAvg < roster[j].getAVG()){
+                    maxAvg = roster[j].getAVG();
+                    maxPlayer = roster[j].getName();
+                    index = j;
+                }
+            }
+            orderedRoster[i] = roster[index];
+            alreadyAdded.add(maxPlayer);
+        }
+
+        for (int i = 0; i < orderedRoster.length; i++){
+            System.out.println(orderedRoster[i].getName() + " : " + orderedRoster[i].getAVG());
+        }
+    }
+
+    public void makeTeams(String[] players, Team team1, Team team2){
+        Player[] nextGame = new Player[10];
+        int counter = 0;
+        for (int i = 0; i < roster.length; i++){
+            for (int j = 0; j < players.length; j++) {
+                Player selected = roster[i];
+                if (selected.getName().equals(players[j])) {
+                    nextGame[counter] = new Player(players[j], selected.getES(), selected.getBS(), selected.getSHO(), selected.getDEF(), selected.getPHY(), selected.getCON());
+                    counter += 1;
+                }
+            }
+        }
+
+        int ar[] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+        shuffleArray(ar);
+
+        for (int i = 0; i < ar.length; i++){
+            if (i < 5) team1.setPlayer( i, nextGame[ar[i]]);
+            else team2.setPlayer(i-5, nextGame[ar[i]]);
+        }
+
+        Random rand = new Random();
+        while (team2.getTotal() > team1.getTotal() + 10 || team2.getTotal() < team1.getTotal() - 10){
+            int index1 = rand.nextInt(5);
+            int index2 = rand.nextInt(5);
+            team1.switchPlayers(index1, index2, team2);
+        }
+    }
+
+    private static void shuffleArray(int[] ar)
+    {
+        // If running on Java 6 or older, use `new Random()` on RHS here
+        Random rnd = new Random();
+        for (int i = ar.length - 1; i > 0; i--)
+        {
+            int index = rnd.nextInt(i + 1);
+            // Simple swap
+            int a = ar[index];
+            ar[index] = ar[i];
+            ar[i] = a;
         }
     }
 }
